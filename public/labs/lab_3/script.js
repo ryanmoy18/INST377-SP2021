b672a84ed032684cca1e099d2d2dd96178ff9a8d
ryanmoy18 <ryan.moy99@gmail.com>
2021-02-12 21:11:36 -0500

    Lab 3

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,1 +1,17 @@
-/* Put your javascript in here */
+let width = 130;
+let visible = 3;
+let sliderList = onigiri.querySelector("ul");
+let slides = onigiri.querySelectorAll("li");
+let curr = 0;
+
+onigiri.querySelector(".previous").onclick = function () {
+  curr += width * visible;
+  curr = Math.min(curr, 0);
+  sliderList.style.marginLeft = curr + "px";
+};
+
+onigiri.querySelector(".next").onclick = function () {
+  curr -= width * visible;
+  curr = Math.max(curr, -width * (slides.length - visible));
+  sliderList.style.marginLeft = curr + "px";
+};
